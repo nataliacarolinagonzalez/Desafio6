aa770b5fedc40e577302f02891ef081722b6ec05
nataliacarolinagonzalez <natalia.carolina.gonzalez@gmail.com>
2023-07-23 20:43:37 -0300

    Primer script

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("Ejercicio a")
+var cantidadDeGatos = 10
+//var cantidadDeGatos = 1
+//var cantidadDeGatos = 4
+
+let i = 1
+
+while (i <= cantidadDeGatos){
+    console.log("Gato #" + i + ": 😺" )
+    i++
+    if (i <= cantidadDeGatos){
+        console.log("Gato #" + i + ": 😸" )
+        i++
+        if (i <= cantidadDeGatos){
+            console.log("Gato #" + i + ": 😹" )
+            i++
+        }
+    }   
+}
+
