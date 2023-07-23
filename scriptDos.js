8c186f8c517af83c4c0967f1d366ab79ef20f91b
nataliacarolinagonzalez <natalia.carolina.gonzalez@gmail.com>
2023-07-23 20:44:29 -0300

    Segundo script

---
+++ scriptDos.js
@@ -0,0 +1,16 @@
+console.log("Ejercicio b")
+
+var cantidadDeGatos = 2;
+var cantidadDePasos = 8;
+let pasos = "🐾"
+var cantidadDeGatos = 5;
+var cantidadDePasos = 3;
+//var cantidadDeGatos = 10;
+//var cantidadDePasos = 4;
+
+let j = 1
+while (j <= cantidadDeGatos){
+    console.log("Gato #" + j + ": 🐈" + pasos.repeat(cantidadDePasos) )
+    j++
+}
+
